b041399220855494df28e100340b813727df7dc7
jm <jm@macbook.local>
2021-05-19 19:57:17 -0500

    update

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ exports.play = (req, res) => {
         // .add(1, "days")
         .milliseconds(0)
         .seconds(0)
-        .minutes(55)
-        .hours(19)
+        .minutes(5)
+        .hours(20)
         .subtract(ZERO_O_CLOCK_AT, "seconds")
         .subtract(parseInt(process.env.LATENCY_TO_DISCORD_MS), "milliseconds");
 
